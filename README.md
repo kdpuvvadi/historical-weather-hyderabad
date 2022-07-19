f2c510a30797ab827c13afbd4b6cfe53ed0a4bcd
KD Puvvadi <kdpuvvadi@users.noreply.github.com>
2022-07-19 11:37:59 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Historical Weather data Hyderabad
+
+---
+
+<!-- ![image](https://user-images.githubusercontent.com/81188/93883886-755c1980-fce2-11ea-824f-cb97a936290e.png) -->
+
+[Web visual](https://weather-hyderabad.puvvadi.me/)
+
+Favicon by [Just UI](https://www.iconfinder.com/icons/669958/cloud_forecast_sun_weather_icon)
---
+++ assets/js/today.js
@@ -0,0 +1,83 @@
+var date = new Date();
+var dd = String(date.getDate()).padStart(2, '0');
+var mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
+var month = date.toLocaleString('default', { month: 'long' });
+var yyyy = date.getFullYear();
+today = yyyy + '' + mm + '' + dd;
+
+function getDirection(angle) {
+    var directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
+    var index = Math.round(((angle %= 360) < 0 ? angle + 360 : angle) / 45) % 8;
+    return directions[index];
+}
+
+function getVisibility(visibilityM) {
+    var visibilityKM = (visibilityM / 1000).toFixed(2);
+    return visibilityKM;
+}
+
+function getInPSI(pressure) {
+    var pressureInPSI = (pressure / 68.948).toFixed(2);
+    return pressureInPSI;
+}
+
+var dateToday = dd + ' ' + month + ' ' + yyyy ;
+    document.getElementById("dateofday").innerText = dateToday
+
+rawDataPath = '/assets/data/' + today + '.json'
+
+function rawPath() {
+    window.open(rawDataPath, "_blank")
+}
+
+fetch(`./assets/data/${today}.json`)
+    .then(response => response.json())
+    .then(data => {
+        document.querySelector("#temp").innerText = data.main.temp + ' °C'
+    })
+
+fetch(`./assets/data/${today}.json`)
+    .then(response => response.json())
+    .then(data => {
+        document.querySelector("#tempmin").innerText = data.main.temp_min + ' °C'
+    })
+
+fetch(`./assets/data/${today}.json`)
+    .then(response => response.json())
+    .then(data => {
+        document.querySelector("#tempmax").innerText = data.main.temp_max + ' °C'
+    })
+fetch(`./assets/data/${today}.json`)
+    .then(response => response.json())
+    .then(data => {
+        document.querySelector("#tempfeel").innerText = data.main.feels_like + ' °C'
+    })
+fetch(`./assets/data/${today}.json`)
+    .then(response => response.json())
+    .then(data => {
+        document.querySelector("#humidity").innerText = data.main.humidity + '%'
+    })
+
+fetch(`./assets/data/${today}.json`)
+    .then(response => response.json())
+    .then(data => {
+        document.querySelector("#windspeed").innerText = data.wind.speed + ' m/s' +  ' ' + getDirection(data.wind.deg)
+    })
+
+fetch(`./assets/data/${today}.json`)
+    .then(response => response.json())
+    .then(data => {
+        document.querySelector("#vis").innerText =  getVisibility(data.visibility) + ' KM'
+    })
+    
+fetch(`./assets/data/${today}.json`)
+    .then(response => response.json())
+    .then(data => {
+        document.querySelector("#press").innerText = getInPSI(data.main.pressure) + ' psi'
+    })
+
+fetch(`./assets/data/${today}.json`)
+    .then(response => response.json())
+    .then(data => {
+        document.querySelector("#clouds").innerText = data.clouds.all + '%'
+    })
